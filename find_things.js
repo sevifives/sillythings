220944158d27a07717e0e48724dc4016e59e035d
sevifives <jdooley@kungfeud.com>
2018-09-26 23:00:21 -0700

    explain totesFind

--- find_things.js
+++ find_things.js
@@ -1,3 +1,15 @@
+/*
+	Took a bit to remember what this did...
+
+	finds unique sets of numbers from an array that sum to the total but will
+	exclude multiple matches using the first number combined with a second number
+
+	ex:
+	totesFind([2,1,1,2,1],3)
+	[[0,2,1],[1,1,2],[3,2,1]]
+
+	returns a collection of tuples (index)
+*/
 function totesFind(inp, total) {
 	var remaining = inp.slice(0), // clone
 		i,l, head, un, ret=[],
@@ -13,10 +25,12 @@ function totesFind(inp, total) {
 
 			if ((total === (head+x)) && !uniques[un]) {
 				uniques[un] = true;
-				ret.push([head,x]);
+				ret.push([i,head,x]);
 			}
 		});
 	}
 
 	return ret;
 }
+
+console.log(totesFind([1,3,5,7,3,2,6,7,89,5,4,3,2,5,6,7,4,2,3,4,6,7],6));
